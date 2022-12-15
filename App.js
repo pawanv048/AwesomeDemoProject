669c3702429600e6fb6008dbdab3b9d66b739962
pawanv048 <pawanv048@gmail.com>
2022-12-15 10:25:09 +0530

    first commit

--- App.js
+++ App.js
@@ -1,26 +1,435 @@
-import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
-import COLORS from './src/constants/color';
-import HomeScreen from './src/views/screens/HomeScreen';
-import DetailsScreen from './src/views/screens/DetailsScreen';
-import WelcomeScreen from './src/views/screens/WelcomeScreen';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { createContext, useContext, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LinearGradient from 'react-native-linear-gradient';
 
-const Stack = createNativeStackNavigator();
+import {
+  Button,
+  View,
+  Text,
+  Dimensions,
+  Image,
+  FlatList,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+  Modal,
+  SafeAreaView,
+  Pressable,
+  Linking,
+  Platform
+} from 'react-native';
 
-const App = () => {
+const { width, height } = Dimensions.get('screen');
+const Profile_men = 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80';
+const img_1 = 'https://uxwing.com/wp-content/themes/uxwing/download/user-interface/plus-icon.png'
+const data = [
+  { id: 1, name: 'pawan',email: 'item1@gmail.com',salary: "5 lpa",phone: 12345, position: 'Mobile dev', picture: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"},
+  { id: 2, name: 'item1',email: 'item2@gmail.com',salary: "6 lpa",phone: 12645, position: 'web dev', picture: "https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"},
+  { id: 3, name: 'item2',email: 'item3@gmail.com',salary: "4 lpa",phone: 14345, position: 'web dev', picture: "https://images.unsplash.com/photo-1500048993953-d23a436266cf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80"},
+  { id: 4, name: 'item3',email: 'item4@gmail.com',salary: "7 lpa",phone: 19345, position: 'dev ops', picture: "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=761&q=80"},
+  { id: 5, name: 'item4',email: 'item5@gmail.com',salary: "8 lpa",phone: 12545, position: 'student', picture: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"},
+]
+
+
+const NotesContext = createContext()
+
+const NoteProvider = ({ children }) => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='HomeScreen' component={HomeScreen}/>
-        <Stack.Screen name='DetailsScreen' component={DetailsScreen}/>
-        <Stack.Screen name='WelcomeScreen' component={WelcomeScreen}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <NotesContext.Provider value={10}>
+      {children}
+    </NotesContext.Provider>
   )
 }
 
-export default App
+// Create Employee
 
-const styles = StyleSheet.create({})
+const CreateEmployee = ({ navigation }) => {
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+  const [salary, setSalary] = useState('')
+  const [modalVisible, setModalVisible] = useState(false);
+
+  return (
+    <SafeAreaView>
+      <TextInput
+        value={name}
+        onChangeText={(text) => setName(text)}
+        style={styles.inputs}
+        placeholder='Enter Name'
+
+      />
+      <TextInput
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+        style={styles.inputs}
+        placeholder='Enter Email'
+      />
+      <TextInput
+        value={phone}
+        onChangeText={(text) => setPhone(text)}
+        style={styles.inputs}
+        placeholder='Enter Phone'
+      />
+      <TextInput
+        value={salary}
+        onChangeText={(text) => setSalary(text)}
+        style={styles.inputs}
+        placeholder='Enter Salary'
+      />
+      <TouchableOpacity
+        onPress={() => setModalVisible(true)}
+        style={styles.btn}
+      >
+        <Text style={styles.txt}>Upload Image</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Profile')}
+        style={styles.btn}
+      >
+        <Text style={styles.txt}>Save</Text>
+      </TouchableOpacity>
+      <Modal
+        style={{ width: width, height: height }}
+        animationType={'slide'}
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          console.log('Modal has been closed.');
+        }}>
+        {/*All views of Modal*/}
+        {/*Animation can be slide, slide, none*/}
+        <View style={styles.modal}>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <View style={styles.modalBtn}>
+              <Button
+                title="Camera"
+                onPress={() => {
+                  setModalVisible(!modalVisible);
+                }}
+              />
+            </View>
+            <View style={styles.modalBtn}>
+              <Button
+                title="Gallery"
+                onPress={() => {
+                  setModalVisible(!modalVisible);
+                }}
+              />
+            </View>
+          </View>
+          <View style={[styles.modalBtn, { marginTop: 50, alignSelf: 'center' }]}>
+            <Button
+              title="cancel"
+              onPress={() => {
+                setModalVisible(!modalVisible);
+              }}
+            />
+          </View>
+        </View>
+
+      </Modal>
+    </SafeAreaView>
+  )
+}
+
+
+// PROFILE SCREEN
+
+const ProfileScreen = (props) => {
+
+  const {id,name,position,salary,phone,picture,email} = props.route.params.item 
+  //console.log('UserDetails =>', props.route.params.item)
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={{ width: '100%', backgroundColor: 'blue', height: 100 }} />
+      <View style={{ alignItems: 'center', marginTop: -40 }}>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            borderRadius: 50,
+            marginRight: 10
+          }}
+          source={{ uri: picture }}
+        />
+      </View>
+      <View style={styles.userName}>
+        <Text style={styles.userTxt}>{name}</Text>
+      </View>
+      <View style={[styles.userName, { marginVertical: 5 }]}>
+        <Text style={styles.userTxt}>{position}</Text>
+      </View>
+      <View style={{ marginVertical: 20 }}>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          style={styles.userDetails}>
+          <Text>{email}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          style={[styles.userDetails, { marginVertical: 15 }]}>
+          <Text>{phone}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          style={styles.userDetails}>
+          <Text>{salary}</Text>
+        </TouchableOpacity>
+
+        <View style={{ marginVertical: 40, marginHorizontal: 20, flexDirection: 'row', justifyContent: 'space-between' }}>
+          <TouchableOpacity
+            style={{
+              backgroundColor: 'blue',
+              padding: 15,
+              borderRadius: 5,
+              width: '30%',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}>
+            <Text style={{ color: 'white', fontSize: 15 }}>Edit</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={{
+              backgroundColor: 'blue',
+              padding: 15,
+              borderRadius: 5,
+              width: '30%',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text style={{ color: 'white', fontSize: 15 }}>Delete</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+// const renderList = data.map((item) => {
+//   return (
+//     <View 
+//       key={item.id}
+//       style={{
+//       //width: width,
+//       height: 100,
+//       backgroundColor: '#b1afb3',
+//       paddingHorizontal: 20,
+//       paddingVertical: 5,
+//       flexDirection: 'row',
+//       alignItems: 'center',
+//       margin: 10,
+//       borderRadius:5
+//     }}
+//       
+//     >
+//       <Image style={{
+//         width: 50,
+//         height: 50,
+//         borderRadius: 25,
+//         marginRight: 10
+//       }}
+//         source={{ uri: Profile_men }}
+//       />
+//       <View>
+//         <Text style={{
+//           fontWeight: '800'
+//         }}>{item.name}</Text>
+//         <Text style={{
+//           fontWeight: '800'
+//         }}>{item.position}</Text>
+//       </View>
+//     </View>
+//   )
+// }) 
+
+
+
+const headerOptions = {
+  title: 'My Sweet Home',
+  headerTintColor: 'white',
+  headerStyle: {
+    backgroundColor: 'blue'
+  }
+}
+
+
+// HOME SCREEEN
+function HomeScreen(props) {
+  //const data = useContext(NotesContext)
+  const renderItems = ({ item, index }) => {
+    return (
+      <TouchableOpacity
+        onPress={() => props.navigation.navigate('Profile',{item})}
+        activeOpacity={1}
+        style={{
+          //width: width,
+          height: 100,
+          backgroundColor: '#b1afb3',
+          paddingHorizontal: 20,
+          paddingVertical: 5,
+          flexDirection: 'row',
+          alignItems: 'center',
+          margin: 10,
+          borderRadius: 5
+        }}
+        key={item.id}
+      >
+        <Image style={{
+          width: 50,
+          height: 50,
+          borderRadius: 25,
+          marginRight: 10
+        }}
+          source={{ uri: item.picture }}
+        />
+        <View>
+          <Text style={{
+            fontWeight: '800'
+          }}>{item.name}</Text>
+          <Text style={{
+            fontWeight: '800'
+          }}>{item.position}</Text>
+  
+        </View>
+      </TouchableOpacity>
+    )
+  }
+
+  return (
+    <View>
+      {/* {renderList} */}
+      <FlatList
+        data={data}
+        renderItem={renderItems}
+        keyExtractor={item => item.id}
+        bounces={false}
+      />
+      <TouchableOpacity
+        onPress={() => props.navigation.navigate('CreateEmployee')}
+        style={{
+          backgroundColor: 'red',
+          padding: 10,
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: 40,
+          borderRadius: 20,
+          position: 'absolute',
+          right: 20,
+          bottom: -100
+        }}>
+        <Image source={{ uri: img_1 }}
+          style={{
+            height: 20,
+            width: 20,
+            tintColor: 'white'
+          }} />
+        {/* <Text>button</Text> */}
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function DetailsScreen({ navigation }) {
+  const data = useContext(NotesContext)
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.navigate('Details')}
+      />
+      {/* <Text>{data}</Text> */}
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NoteProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} options={headerOptions} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+          <Stack.Screen
+            name="CreateEmployee"
+            component={CreateEmployee}
+            options={{ ...headerOptions, title: 'Create Employee' }} />
+          <Stack.Screen name='Profile' component={ProfileScreen} options={{ ...headerOptions, title: 'Profile' }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NoteProvider>
+  );
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  inputs: {
+    margin: 10,
+    borderWidth: 1,
+    borderColor: 'blue',
+    borderRadius: 5,
+    width: '90%',
+    height: 50,
+    paddingLeft: 15
+  },
+  btn: {
+    width: '90%',
+    height: 50,
+    backgroundColor: 'blue',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    margin: 10
+  },
+  txt: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: 'white'
+  },
+  modal: {
+    //flex: 1,
+    //alignItems: 'center',
+    backgroundColor: 'grey',
+    paddingVertical: 40,
+    width: '100%',
+    //paddingHorizontal: 20,
+    //flexDirection: 'row',
+    position: 'absolute',
+    bottom: 0,
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20
+  },
+  text: {
+    color: '#3f2949',
+    marginTop: 10,
+  },
+  modalBtn: {
+    backgroundColor: 'white',
+    width: '40%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 20
+  },
+  userName: {
+    alignItems: 'center',
+    marginVertical: 10
+  },
+  userTxt: {
+    fontSize: 18,
+    fontWeight: '700'
+  },
+  userDetails: {
+    marginHorizontal: 20,
+    backgroundColor: 'grey',
+    borderRadius: 5,
+    padding: 15
+  }
+})
